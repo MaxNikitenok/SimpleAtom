6e05888f42adf76ea7d4192ae698f825dfde4e4d
Max Nikitenok <xxx250790@gmail.com>
2024-02-17 02:12:14 +0300

    init: start dev

---
+++ simpleatom/src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './normalize.css';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Main } from './pages/Main.jsx';
+import { App } from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/main" element={<Main />} />
+          <Route path="/simpleatom" element={<Main />} />
+          <Route path="/whySimpleatom" element={<Main />} />
+          <Route path="/aboutUs" element={<Main />} />
+          <Route path="/contactUs" element={<Main />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ simpleatom/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import logo from '../assets/Logo.png'
+import styles from './Main.module.css';
+
+export const Main = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.main__logo}>
+        <img src={logo} alt="logo" />
+      </div>
+      <h1 className={styles.main__description}>SimpleAtom is an investment management company specializing in cryptocurrency trading</h1>
+      <button className={styles.main__button}>Read more</button>
+    </main>
+  );
+};
